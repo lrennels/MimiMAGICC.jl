71b1b39813bf3b0b30989eca2f7b0caf28798a58
David Anthoff <anthoff@berkeley.edu>
2019-08-23 16:30:05 -0700

    Turn into package

---
+++ src/components/radiativeforcing_magicc.jl
@@ -0,0 +1,26 @@
+using Mimi
+
+@defcomp radiativeforcing begin
+    deltat = Parameter()
+    rf_co2 = Parameter(index=[time])
+    rf_aerosol = Parameter(index=[time])
+    QMeth = Parameter(index=[time]) #direct radiative forcing from atmospheric ch4 concentrations only
+    rf_O3 = Parameter(index=[time]) #radiative forcing from tropospheric ozone (direct O3 and ch4-induced ozone production)
+    QCH4H2O = Parameter(index=[time]) #indirect CH4 forcing due to production of stratospheric H20 from CH4 oxidation
+    rf_other = Parameter(index=[time])
+    alpha = Parameter()
+    rf = Variable(index=[time])
+end
+
+function run_timestep(s::radiativeforcing, t::Int)
+    v = s.Variables
+    p = s.Parameters
+
+if t==1
+    v.rf[t] = 0.0
+    else
+
+    v.rf[t] = p.rf_co2[t] + p.QMeth[t]+ p.QCH4H2O[t] + p.rf_other[t] + (p.alpha * p.rf_aerosol[t]) + p.rf_O3[t]
+    end    
+end
+
---
+++ src/components/rfo3_magicc.jl
@@ -0,0 +1,47 @@
+using Mimi
+
+@defcomp rfo3magicc begin
+    deltat          = Parameter()
+    OZ00CH4         = Parameter() # 2000 OZONE FORCING DUE TO CH4 (ORIG .168, NOW .161)
+    TROZSENS        = Parameter() #TROP OZONE FORCING SENSITIVITY (TAR=0.042, EMPIRICAL=0.0389)
+    OZNOX           = Parameter() #NOX TERM FACTOR IN OZONE EQU (ORIG 0.17, NOW 0.125)
+    OZCO            = Parameter() #CO TERM FACTOR IN OZONE EQU (ORIG .0014, NOW .0011)
+    OZVOC           = Parameter() #VOC TERM FACTOR IN OZONE EQU (ORIG .0042, NOW .0033)
+    NOX_emissions   = Parameter(index=[time]) # Global NOx emissions in Mt/yr
+    CO_emissions    = Parameter(index=[time]) # Global CO emissions in Mt/yr
+    NMVOC_emissions = Parameter(index=[time]) # Global non-methane VOC emissions in Mt/yr
+    FOSSHIST        = Parameter(index=[time])
+    QCH4OZ          = Parameter(index=[time]) #QCH4OZ IS THE TROP OZONE FORCING FROM CH4 CHANGES.
+    QOZ             = Variable(index=[time]) #TROPOSPHERIC OZONE FORCING NOT ASSOCIATED WITH CH4.
+    rf_O3           = Variable(index=[time]) #Total radiative forcing tropospheric ozone (from direct O3 and Ch4 oxidation)
+end
+
+
+function run_timestep(s::rfo3magicc, t::Int)
+    v = s.Variables
+    p = s.Parameters
+
+    QREF = 0.33 - p.OZ00CH4
+
+    #Use concentrations for 1765-1999.
+    if t <= 235 #index for year 1999
+        FOSS0 = p.FOSSHIST[1]
+        v.QOZ[t] = QREF *(p.FOSSHIST[t] - FOSS0) / (p.FOSSHIST[235] - FOSS0)
+    else
+
+        DQOZ = v.QOZ[235] - v.QOZ[234]
+
+        #Calculate change in emissions for NOX, CO, and NMVOC
+        DENOX = p.NOX_emissions[t] - p.NOX_emissions[236]
+        DECO = p.CO_emissions[t] - p.CO_emissions[236]
+        DEVOC = p.NMVOC_emissions[t] - p.NMVOC_emissions[236]
+
+        #Non methane OZ
+        v.QOZ[t] = QREF + DQOZ + p.TROZSENS*(p.OZNOX*DENOX + p.OZCO*DECO + p.OZVOC*DEVOC)
+
+    end
+
+    #Total trop O3 Radiative forcing (from direct O3 and indirect CH4 effect)
+    v.rf_O3[t] = v.QOZ[t] + p.QCH4OZ[t]
+
+ end
