db26ff3decb7f176ca9d6db17feb357681e3b507
David Anthoff <anthoff@berkeley.edu>
2019-08-27 11:47:23 -0700

    Add main package code file

---
+++ src/MimiMAGICC.jl
@@ -0,0 +1,12 @@
+module MimiMAGICC
+
+using Mimi
+
+include("components/ch4cycle_magicc_mcmc_emissions_version.jl")
+include("components/ch4cycle_magicc.jl")
+include("components/radiativeforcing_magicc.jl")
+include("components/rfch4_magicc_old.jl")
+include("components/rfco2_old.jl")
+include("components/rfo3_magicc.jl")
+
+end
