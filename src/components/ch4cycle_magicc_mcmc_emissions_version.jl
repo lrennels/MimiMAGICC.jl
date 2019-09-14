7acfdad843f5b3f683d13cd477496af9be9d2008
Frank <frankerrickson@berkeley.edu>
2019-09-14 14:58:26 -0700

    Clean up CH4 cycle code and comments

--- src/components/ch4cycle_magicc_mcmc_emissions_version.jl
+++ src/components/ch4cycle_magicc_mcmc_emissions_version.jl
@@ -1,148 +1,132 @@
-@defcomp ch4cyclemagicc begin
-    
-    index_2000::Int64 = Parameter() # Index to access values in the year 2000 (just for convenience).
-    BBCH4           = Parameter() #Tg(CH4)/ppb unit conversion between emissions and concentrations Note:(1 Teragram = 1 Megatonne)
-    ANOX            = Parameter()
-    ACO             = Parameter()
-    AVOC            = Parameter()
-    TAUINIT         = Parameter()
-    SCH4            = Parameter()
-    TAUSOIL         = Parameter()
-    TAUSTRAT        = Parameter()
-    GAM             = Parameter()
-    CH4_natural     = Parameter()
-    fffrac          = Parameter()               #fossil fuel fraction of total CH4 emissions
-    CH4_0           = Parameter() #Initial CH4 concentration.
-    temp            = Parameter(index=[time])
-    CH4_emissions   = Parameter(index=[time])   # Global anthropogenic CH4 emissions in Mt/yr
-    NOX_emissions   = Parameter(index=[time])   # Global NOx emissions in Mt/yr
-    CO_emissions    = Parameter(index=[time])   # Global CO emissions in Mt/yr
-    NMVOC_emissions = Parameter(index=[time])   # Global non-methane VOC emissions in Mt/yr
-    TAU_OH          = Variable(index=[time])   #Lifetime OH sink (years)
-    CH4             = Variable(index=[time]) #Atmospheric concetration of CH4
-    emeth           = Variable(index=[time]) #Additional CO2 emissions due to CH4 oxidation
+# --------------------------------------------------
+# Methane Cycle (with OH calculations).
+# --------------------------------------------------
+
+@defcomp ch4_cycle begin
+
+    BBCH4             = Parameter()             # Tg to ppb unit conversion between emissions and concentrations Note:(1 Teragram = 1 Megatonne).
+    ANOX              = Parameter()             # Hydroxyl lifetime coefficient for nitrogen oxides.
+    ACO               = Parameter()             # Hydroxyl lifetime coefficient for carbon monoxide.
+    AVOC              = Parameter()             # Hydroxyl lifetime coefficient for non-methane volatile organic compounds.
+    SCH4              = Parameter()             # Hydroxyl lifetime coefficient for methane.
+    TAUINIT           = Parameter()             # Initial methane tropospheric lifetime due to hydroxyl abundance (years).
+    TAUSOIL           = Parameter()             # Methane soil sink lifetime (years).
+    TAUSTRAT          = Parameter()             # Methane stratospheric lifetime (years).
+    GAM               = Parameter()             # Hydroxyl lifetime parameter.
+    CH4_natural       = Parameter()             # Natural methane emissions (Mt yr⁻¹).
+    fffrac            = Parameter()             # Fossil fuel fraction of total methane emissions.
+    CH4_0             = Parameter()             # Atmospheric methane pre-industrial concentration (ppb).
+    index_2000::Int64 = Parameter()             # Index to access values in the year 2000 (just for convenience).
+    temperature       = Parameter(index=[time]) # Global surface temperature anomaly (°C).
+    CH4_emissions     = Parameter(index=[time]) # Global anthropogenic methane emissions (Mt yr⁻¹).
+    NOX_emissions     = Parameter(index=[time]) # Global nitrogen oxides emissions (Mt yr⁻¹).
+    CO_emissions      = Parameter(index=[time]) # Global carbon monoxide emissions (Mt yr⁻¹).
+    NMVOC_emissions   = Parameter(index=[time]) # Global non-methane volatile organic compound emissions (Mt yr⁻¹).
+
+    TAU_OH            = Variable(index=[time])  # Methane tropospheric lifetime due to hydroxyl abundance (years).
+    CH4               = Variable(index=[time])  # Atmospheric methane concetration for current period (ppb).
+    emeth             = Variable(index=[time])  # Methane that has been oxidized to carbon dioxide.
+
 
     function run_timestep(p, v, d, t)
-        # Set initial conditions before switching CH₄ emissions cycle on.
-        # Note, period1 CH4 doesn't affect CH4 cycle results (period 2 does), due to subsequent equations needing values from 2 periods back.
-        # Radiative forcing equations use Period 1 CH4 (so set to be the same).
+
+        # Set initial conditions. NOTE: The CH₄ cycle equations require temperature values from the previous two timesteps (t-1 and t-2), so the cycle switches
+            # on starting in timestep 3. Further, timestep 1 CH₄ concentrations do not affect timestep 1 temperature or the results below (but timestep 2 CH₄
+            # concentrations do). We therefore treat period 2 CH₄ concentrations as an initial (and uncertain) condition.
         if t.t <= 2
-            # Initialize first two periods of model (period 1 temp fixed)
-            #v.CH4[1] = 0.0
-
-
-            v.CH4[2] = p.CH4_0 
+            v.CH4[2] = p.CH4_0
             v.emeth[t] = 0.0
         else
-    
-            # Model code had temperature sensitiity relative to temp increase above 2000
+
+            # Calculate changes for temperature sensitivity equation (maintain MAGICC's original scaling relative to increase above 2000).
             if gettime(t) < 2000
                 D_Temp = 0.0
             else
-                DELT00 = 2.0 * p.temp[p.index_2000-1] - p.temp[p.index_2000-2]
-                TX = 2.0 * p.temp[t-1] - p.temp[t-2]
+                DELT00 = 2.0 * p.temperature[p.index_2000-1] - p.temperature[p.index_2000-2]
+                TX = 2.0 * p.temperature[t-1] - p.temperature[t-2]
                 D_Temp = TX - DELT00
             end
-    
-            #Calculate change in emissions for NOX, CO, and NMVOC (relative to period 3 when methane cycle model engages due to t-1 and t-2 terms above)
+
+            # Calculate change in emissions for NOx, CO, and NMVOC (relative to period 3 when CH₄ cycle model engages due to t-1 and t-2 terms above).
             DENOX = p.NOX_emissions[t] - p.NOX_emissions[3]
-            DECO = p.CO_emissions[t] - p.CO_emissions[3]
+            DECO  = p.CO_emissions[t] - p.CO_emissions[3]
             DEVOC = p.NMVOC_emissions[t] - p.NMVOC_emissions[3]
-    
-            #Add natural emissions to methane emissions
+
+            # Add natural CH₄ emissions to anthropogenic CH₄ emissions.
             Total_CH4emissions = p.CH4_emissions[t] + p.CH4_natural
-    
-            #Calcualte Tauother (based on strat and soil lifetimes)
+
+            # Calcualte TAUOTHER term (based on stratospheric and soil sink lifetimes).
             TAUOTHER = 1.0 / (1.0/p.TAUSOIL + 1.0/p.TAUSTRAT)
-    
-         
-            #######################################################
-            #Calculate OH lifetime and CH4 concentration projection
-            #######################################################
-    
+
+            ##########################################################
+            # Iterate to Calculate OH lifetime and CH₄ concentration.
+            # See equations A30-A32 in Meinsshausen et al.(2011).
+            ##########################################################
+
+            # Convert previous timestep concentration to mass.
             B = v.CH4[t-1] * p.BBCH4
-    
-            #Convert historical concentrations to mass
+
+            # Convert historical concentration to mass.
             B00 = p.CH4_0 * p.BBCH4
-    
-            #calculate relative oh lifetime see equation in paper A terms are coefficients given in param list. D terms are differences in emissions
+
+            # Account for other gases on OH abundance and tropospheric methane lifetime.
             AAA = exp(p.GAM * (p.ANOX*DENOX + p.ACO*DECO + p.AVOC*DEVOC))
-    
-            #Change sign of ch4 parameter (does it do something else?)
+
+            # Multiply two parameters together.
             X = p.GAM * p.SCH4
-    
-            #Calculate Tau_oh
+
+            # Multiply initial tropospheric lifetime by AAA.
             U = p.TAUINIT * AAA
-    
-            ##########################################
-            #FIRST ITERATION
-            ##########################################
-    
-            #B defined above
+
+            # Iterate to calculate a final CH₄ tropospheric lifetime (accounting for a temperature feedback).
+
+            #----------------------------------------
+            # FIRST ITERATION
+            #----------------------------------------
             BBAR = B
-    
             TAUBAR = U * (BBAR/B00) ^ X
-    
             TAUBAR = p.TAUINIT / (p.TAUINIT/TAUBAR + 0.0316 * D_Temp)
-    
             DB1 = Total_CH4emissions - (BBAR/TAUBAR) - (BBAR/TAUOTHER)
-    
             B1 = B + DB1
-    
-    
-            ##########################################
-            #SECOND ITERATION
-            ##########################################
+
+            #----------------------------------------
+            # SECOND ITERATION
+            #----------------------------------------
             BBAR = (B + B1) / 2.0
-    
             TAUBAR = U * (BBAR/B00) ^ X
-    
             TAUBAR = TAUBAR * (1.0 - 0.5 * X * DB1/B)
-    
             TAUBAR = p.TAUINIT / (p.TAUINIT/TAUBAR + 0.0316 * D_Temp)
-    
             DB2 = Total_CH4emissions - (BBAR/TAUBAR) - (BBAR/TAUOTHER)
-    
             B2 = B + DB2
-    
-            ##########################################
-            #THIRD ITERATION
-            ##########################################
+
+            #----------------------------------------
+            # THIRD ITERATION
+            #----------------------------------------
             BBAR = (B + B2) / 2.0
-    
             TAUBAR = U * (BBAR/B00) ^ X
-    
             TAUBAR = TAUBAR * (1.0 - 0.5 * X * DB2/B)
-    
-    
             TAUBAR = p.TAUINIT / (p.TAUINIT/TAUBAR + 0.0316 * D_Temp)
-    
             DB3 = Total_CH4emissions - (BBAR/TAUBAR) - (BBAR/TAUOTHER)
-    
             B3 = B + DB3
-    
-            ##########################################
-            #FOURTH ITERATION
-            ##########################################
+
+            #----------------------------------------
+            # FOURTH ITERATION
+            #----------------------------------------
             BBAR = (B + B3) / 2.0
-    
             TAUBAR = U * (BBAR/B00) ^ X
-    
             TAUBAR = TAUBAR * (1.0 - 0.5 * X * DB3/B)
-    
             TAUBAR = p.TAUINIT / (p.TAUINIT/TAUBAR + 0.0316 * D_Temp)
-    
             DB4 = Total_CH4emissions - (BBAR/TAUBAR) - (BBAR/TAUOTHER)
-    
             B4 = B + DB4
-    
+
+            # Calculate CH₄ tropospheric lifetime.
             v.TAU_OH[t] = TAUBAR
-    
+
+            # Calculate CH₄ concentration.
             v.CH4[t] = B4 / p.BBCH4
-    
-            #Calculate additional CO2 emissions due to CH4 oxidation
+
+            #Calculate additional CO₂ emissions due to CH₄ oxidation.
             v.emeth[t] = p.fffrac * 0.0020625 * (v.CH4[t] - 700.0) / p.TAUINIT
         end
-    end    
-
+    end
 end
