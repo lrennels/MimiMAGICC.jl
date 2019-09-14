b4ccb3a445058f3bbdeb7610c88b7c20b44656bc
Frank <frankerrickson@berkeley.edu>
2019-09-14 15:20:42 -0700

    Remove old CO2 radiative forcing file

--- src/components/rfco2_old.jl
+++
@@ -1,9 +0,0 @@
-@defcomp rfco2 begin
-    atmco2 = Parameter(index=[time])
-    rf_co2 = Variable(index=[time])
-
-    function run_timestep(p, v, d, t)
-        v.rf_co2[t] = 3.7 * log(p.atmco2[t]/p.atmco2[1])/log(2.0)
-    end
-    
-end
