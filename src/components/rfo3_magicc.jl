06d3c0e9455d4843176754b9e9da1d4d5b66f75e
Frank <frankerrickson@berkeley.edu>
2019-09-14 17:13:24 -0700

    Clean and organize tropospheric ozone forcing file

--- src/components/rfo3_magicc.jl
+++ src/components/rfo3_magicc.jl
@@ -1,42 +1,50 @@
-@defcomp rfo3magicc begin
-    deltat          = Parameter()
-    OZ00CH4         = Parameter() # 2000 OZONE FORCING DUE TO CH4 (ORIG .168, NOW .161)
-    TROZSENS        = Parameter() #TROP OZONE FORCING SENSITIVITY (TAR=0.042, EMPIRICAL=0.0389)
-    OZNOX           = Parameter() #NOX TERM FACTOR IN OZONE EQU (ORIG 0.17, NOW 0.125)
-    OZCO            = Parameter() #CO TERM FACTOR IN OZONE EQU (ORIG .0014, NOW .0011)
-    OZVOC           = Parameter() #VOC TERM FACTOR IN OZONE EQU (ORIG .0042, NOW .0033)
-    NOX_emissions   = Parameter(index=[time]) # Global NOx emissions in Mt/yr
-    CO_emissions    = Parameter(index=[time]) # Global CO emissions in Mt/yr
-    NMVOC_emissions = Parameter(index=[time]) # Global non-methane VOC emissions in Mt/yr
-    FOSSHIST        = Parameter(index=[time])
-    QCH4OZ          = Parameter(index=[time]) #QCH4OZ IS THE TROP OZONE FORCING FROM CH4 CHANGES.
-    QOZ             = Variable(index=[time]) #TROPOSPHERIC OZONE FORCING NOT ASSOCIATED WITH CH4.
-    rf_O3           = Variable(index=[time]) #Total radiative forcing tropospheric ozone (from direct O3 and Ch4 oxidation)
+# --------------------------------------------------------
+# Tropospheric Ozone Concentration and Radiative Forcing.
+# --------------------------------------------------------
+
+@defcomp rf_o3 begin
+
+    OZ00CH4           = Parameter()             # Year 2000 tropospheric ozone forcing due to methane (Wm⁻²).
+    TROZSENS          = Parameter()             # Tropospheric ozone radiative efficiency factor.
+    OZNOX             = Parameter()             # Sensitivity coefficient of tropospheric ozone to nitrogen oxides emissions.
+    OZCO              = Parameter()             # Sensitivity coefficient of tropospheric ozone to carbon monoxide emissions.
+    OZVOC             = Parameter()             # Sensitivity coefficient of tropospheric ozone to Non-methane volatile organic compound emissions.
+    index_2000::Int64 = Parameter()             # Index to access values in the year 2000 (just for convenience).
+    NOx_emissions     = Parameter(index=[time]) # Nitrogen oxides emissions (Mt yr⁻¹).
+    CO_emissions      = Parameter(index=[time]) # Carbon monoxide emissions (Mt yr⁻¹).
+    NMVOC_emissions   = Parameter(index=[time]) # Non-methane volatile organic compound emissions (Mt yr⁻¹).
+    FOSSHIST          = Parameter(index=[time]) # Historical fossil carbon dioxide emissions values used to calculate pre-2000 non-methane ozone forcing.
+    QCH4OZ            = Parameter(index=[time]) # Indirect forcing effect from methane due to methane-induced ozone production (Wm⁻²).
+
+    QOZ               = Variable(index=[time])  # Tropospheric ozone radiative forcing not attributed to methane (Wm⁻²).
+    rf_O₃             = Variable(index=[time])  # Total radiative forcing from tropospheric ozone (Wm⁻²).
+
 
     function run_timestep(p, v, d, t)
+
+        # Set a reference value that subtracts out reference CH₄ contribution.
         QREF = 0.33 - p.OZ00CH4
-    
-        #Use concentrations for 1765-1999.
-        if t <= 235 #index for year 1999
+
+        # The CH₄ emission cycle was converted to run on emissions for all time periods (to use concentration observations during calibration).
+        # The O₃ cycle follows the original version of MAGICC by switching to emissions inputs starting in 2000.
+        if gettime(t) < 2000
             FOSS0 = p.FOSSHIST[1]
-            v.QOZ[t] = QREF *(p.FOSSHIST[t] - FOSS0) / (p.FOSSHIST[235] - FOSS0)
+            v.QOZ[t] = QREF *(p.FOSSHIST[t] - FOSS0) / (p.FOSSHIST[p.index_2000-1] - FOSS0)
         else
-    
-            DQOZ = v.QOZ[235] - v.QOZ[234]
-    
-            #Calculate change in emissions for NOX, CO, and NMVOC
-            DENOX = p.NOX_emissions[t] - p.NOX_emissions[236]
-            DECO = p.CO_emissions[t] - p.CO_emissions[236]
-            DEVOC = p.NMVOC_emissions[t] - p.NMVOC_emissions[236]
-    
-            #Non methane OZ
+
+            # Calcualte forcing difference in final periods before switching to emissions version.
+            DQOZ = v.QOZ[p.index_2000-1] - v.QOZ[p.index_2000-2]
+
+            # Calculate change in emissions for NOX, CO, and NMVOC relative to 2000.
+            DENOX = p.NOx_emissions[t]   - p.NOx_emissions[p.index_2000]
+            DECO  = p.CO_emissions[t]    - p.CO_emissions[p.index_2000]
+            DEVOC = p.NMVOC_emissions[t] - p.NMVOC_emissions[p.index_2000]
+
+            # Calculate tropospheric O₃ radiative forcing not attributed to CH₄.
             v.QOZ[t] = QREF + DQOZ + p.TROZSENS*(p.OZNOX*DENOX + p.OZCO*DECO + p.OZVOC*DEVOC)
-    
         end
-    
-        #Total trop O3 Radiative forcing (from direct O3 and indirect CH4 effect)
-        v.rf_O3[t] = v.QOZ[t] + p.QCH4OZ[t]
-    
-     end
-    
+
+        # Calculate total tropospheric O₃ radiative forcing (from direct O₃ and indirect CH₄ effects).
+        v.rf_O₃[t] = v.QOZ[t] + p.QCH4OZ[t]
+    end
 end
