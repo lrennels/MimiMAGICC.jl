0d49b8325d59fa3a8c85cbd8bf09ec62eaa842df
Frank <frankerrickson@berkeley.edu>
2019-09-14 17:13:00 -0700

    Clean and organize methane radiative forcing file

--- src/components/rfch4_magicc_old.jl
+++ src/components/rfch4_magicc_old.jl
@@ -1,35 +1,44 @@
-#The function 'interact' accounts for the overlap in CH4 and N2O in their absoprtion bands
+# --------------------------------------------------
+# Radiative Forcing from Methane.
+# --------------------------------------------------
+
+# Create a function 'interact' to account for the overlap in methane and nitrous oxide in their radiative absoprtion bands.
 function interact(M, N)
     d = 1.0 + 0.636 * ((M * N)/(10^6))^0.75 + 0.007 * (M/(10^3)) * ((M * N)/(10^6))^1.52
     return 0.47 * log(d)
 end
 
-@defcomp rfch4magicc begin
-    N₂O_0              = Parameter() #preindustrial nitrous oxide
-    STRATH2O        = Parameter() #Ratio DELQH2O/DELQCH4, for Strat H2O from CH4 Oxidation
-    OZ00CH4         = Parameter() #2000 OZONE FORCING DUE TO CH4 (ORIG .168, NOW .161)
-    TROZSENS        = Parameter() #TROP OZONE FORCING SENSITIVITY (TAR=0.042, EMPIRICAL=0.0389)
-    OZCH4           = Parameter() #CH4 TERM FACTOR IN OZONE EQU
-    CH4_0           = Parameter()
-    scale_CH₄ = Parameter() #Scaling factor for uncertainty in RF (following FAIR 1.3 approach)
-    CH4             = Parameter(index=[time]) #Natural CH4 emissions (Tgrams)
-    QCH4OZ          = Variable(index=[time]) #Enhancement of QCH4 due to CH4-induced ozone production.
-    QCH4H2O         = Variable(index=[time]) #Additional indirect CH4 forcing due to production of stratospheric H20 from CH4 oxidation
-    QMeth           = Variable(index=[time]) #direct radiative forcing from atmospheric ch4 concentrations only
+@defcomp rf_ch4 begin
+    CH₄_0     = Parameter()             # Pre-industrial atmospheric methane concentration (ppb).
+    N₂O_0     = Parameter()             # Pre-industrial atmospheric nitrous oxide concentration (ppb).
+    STRATH2O  = Parameter()             # Share of direct methane radiative forcing that represents stratospheric water vapor forcing from methane oxidation.
+    TROZSENS  = Parameter()             # Tropospheric ozone radiative efficiency factor.
+    OZCH4     = Parameter()             # Sensitivity coefficient of tropospheric ozone to methane concentration.
+    scale_CH₄ = Parameter()             # Scaling factor for uncertainty in methane radiative forcing.
+    CH₄       = Parameter(index=[time]) # Atmospheric methane concentration (ppb).
+
+    QCH4OZ    = Variable(index=[time])  # Indirect forcing effect from methane due to methane-induced ozone production (Wm⁻²).
+    QCH4H2O   = Variable(index=[time])  # Radiative forcing for stratoshperic water vapor from methane oxidation (Wm⁻²).
+    QMeth     = Variable(index=[time])  # Direct forcing from atmospheric methane concentrations (Wm⁻²).
+
 
     function run_timestep(p, v, d, t)
-        #direct radiative forcing from atmospheric ch4 concentrations only
-        QCH4 = 0.036 * (sqrt(p.CH4[t]) - sqrt(p.CH4_0))
-    
-        v.QMeth[t] = (QCH4 - (interact(p.CH4[t], p.N₂O_0) - interact(p.CH4_0, p.N₂O_0))) * p.scale_CH₄
-    
-        #Calculate indirect CH4 forcing due to production of stratospheric H20 from CH4 oxidation
-        v.QCH4H2O[t] = p.STRATH2O * QCH4
-    
-        #Calculate enhnacement of direct CH4 forcing due to ch4-induced ozone production
-        #NOTE: Old concentration based equation added 2000 O₃ focring due to CH₄. This term was removed for emissions version.
-        v.QCH4OZ[t] = p.TROZSENS * p.OZCH4 * log(p.CH4[t]/p.CH4_0)
-    
-    end
 
+        if is_first(t)
+            # Set initial forcings to 0.
+            v.QMeth[t]   = 0.0
+            v.QCH4H2O[t] = 0.0
+            v.QCH4OZ[t]  = 0.0
+        else
+            # Direct radiative forcing from atmospheric CH₄ concentrations.
+            QCH4 = 0.036 * (sqrt(p.CH₄[t]) - sqrt(p.CH₄_0))
+            v.QMeth[t] = (QCH4 - (interact(p.CH₄[t], p.N₂O_0) - interact(p.CH₄_0, p.N₂O_0))) * p.scale_CH₄
+
+            # Calculate indirect CH₄ forcing due to stratoshperic water vapor production from CH₄ oxidation.
+            v.QCH4H2O[t] = p.STRATH2O * QCH4
+
+            # Calculate indirect CH₄ forcing due to CH₄-induced O₃ production.
+            v.QCH4OZ[t] = p.TROZSENS * p.OZCH4 * log(p.CH₄[t]/p.CH₄_0)
+        end
+    end
 end
